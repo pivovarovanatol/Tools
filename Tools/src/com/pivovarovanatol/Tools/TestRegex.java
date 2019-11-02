7b56aef8b0c08137629d8ff3308c5d8565333c8e
I855719 <I855719@VANN34702302A.amer.global.corp.sap>
2019-11-01 17:00:35 -0700

    test Regex helper class

--- Tools/src/com/pivovarovanatol/Tools/TestRegex.java
+++ Tools/src/com/pivovarovanatol/Tools/TestRegex.java
@@ -22,15 +22,15 @@ public class TestRegex {
 											"(?:(?!(input_size:|olap_fact_rel:|result_size:|output_column_size:|cost:|lowerbound:|subtree_cost:|result_order:|enumerated_by:|addr:|address:)).)*"	+ //required, non-capturing group to skip over the operator_details, up to, but not including, one of the following: 'result_size:', 'subtree_cost:', 'result_order:', 'enumerated_by:' 'address:', '
 											"([LOGICAL|PHYSICAL])?" + // optional group - some oeprators types like [LOGICAL] 
 											"(input_size:(?<inputsize>"+floatingPointNumberRegEx+").*?\\s*)?" + //optional group input_size    //?<inputsize>
-											"(table_location:(?<tablelocation>).*?\\s*)?" + //optional group table_location    //?<tablelocation>
+											"(table_location:(?<tablelocation>).*)?" + //optional group table_location    //?<tablelocation>
 											"(.(LOGICAL|PHYSICAL).*?\\s*)?" + //optional LOGICAL or PHYSICAL operator
 											"(olap_fact_rel:(?<olapfactrel>\\d+).*?\\s*)?" + //optional group olap_fact_rel                      //?<olapfactrel>
-											"(result_size:(?<resultsize>"+floatingPointNumberRegEx+")\\s*)?" + //optional group result_size  //?<resultsize>
-											"(output_column_size:(?<outputcolsize>"+floatingPointNumberRegEx+")\\s*)?" + //optional group output_column_size   //?<outputcolumnsize>
-											"(cost:(?<cost>(DBL_MAX|"+floatingPointNumberRegEx+"))\\s*)?" +  //optional group cost                  //?<cost>
-											"(lowerbound:(?<lowerbound>"+floatingPointNumberRegEx+")\\s*)?" +  //optional group lowerbound      //?<lowerbound>
+											"(result_size:(?<resultsize>"+floatingPointNumberRegEx+").*?\\s*)?" + //optional group result_size  //?<resultsize>
+											"(output_column_size:(?<outputcolsize>"+floatingPointNumberRegEx+").*?\\s*)?" + //optional group output_column_size   //?<outputcolumnsize>
+											"(cost:(?<cost>(DBL_MAX|"+floatingPointNumberRegEx+")).*?\\s*)?" +  //optional group cost                  //?<cost>
+											"(lowerbound:(?<lowerbound>"+floatingPointNumberRegEx+").*?\\s*)?" +  //optional group lowerbound      //?<lowerbound>
 											
-//											TODO: subtree_cost:DBL_MAX is apparently possible to encounter, and probably unhandled											
+							//				TODO: subtree_cost:DBL_MAX is apparently possible to encounter, and probably unhandled											
 											"(subtree_cost:(?<subtreecost>(DBL_MAX|"+floatingPointNumberRegEx+"))\\s*)?" +									//?<subtreecost>  
 											"(result_order:(?<resultorder>.*?)\\s*)?" +  //optional group result_order							//?<resultorder>
 											"(enumerated_by:(?<enumeratedby>.*?)\\s*)?" + //optional group, enumerated_by							//?<enumeratedby>
@@ -40,9 +40,9 @@ public class TestRegex {
 
 		
 		
-		String operator_line = "    # TABLE SOPIBP00PIBP00DIBP00P (CALC VIEW) (0) (opId:L:2) TABLE used cols::  TABLE histo cols::  TABLE key joined cols::  input_size:-1 table_location: vadbzyo:30203|0  [LOGICAL] result_size:-1 rel_addr:0x00007f602886d570";
-		
-		
+		//String operator_line = "    # TABLE SOPIBP00PIBP00DIBP00P (CALC VIEW) (0) (opId:L:2) TABLE used cols::  TABLE histo cols::  TABLE key joined cols::  input_size:-1 table_location: vadbzyo:30203|0  [LOGICAL] result_size:-1 rel_addr:0x00007f602886d570";
+	    
+	    String operator_line = "# PROJECT (opId:L:0) (opId:P:22) ((0, 10000000))  [LOGICAL] result_size:1 lowerbound_cost:1.08892035181818e-05 rel_addr:0x00004d005ed84a30  [PHYSICAL] cost:1.08892035181818e-05 physicalop_addr:0x00004d005ed89db0 at [apbhphana:30003|0]";
 		
 		final Pattern patOperatorLine = Pattern.compile(strOperatorLineRegex); //,Pattern.DOTALL);
 		Matcher matchOperatorLine = patOperatorLine.matcher(operator_line);
@@ -64,8 +64,9 @@ public class TestRegex {
 		System.out.println("Found enumeratedby: " + matchOperatorLine.group("enumeratedby"));
 		System.out.println("Found memoryaddr: " + matchOperatorLine.group("memoryaddr"));
 		System.out.println("Found pushdownblocker: " + matchOperatorLine.group("pushdownblocker"));
+		System.out.println("Found host: " + matchOperatorLine.group("host"));
 
-	
+		
 	}
 	
 
